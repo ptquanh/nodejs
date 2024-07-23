81844341c9576a365a9fa99b3bbdf1a64ce79380
ptquanh <vocsiquocanh@gmail.com>
2024-07-23 23:48:42 +0700

    connectdb and render view

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,7 +4,7 @@ import homeController from "../controllers/homeController";
 let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
-
+    router.get("/about",homeController.getAboutPage);
     return app.use("/",router);
 }
 module.exports = initWebRoutes;
